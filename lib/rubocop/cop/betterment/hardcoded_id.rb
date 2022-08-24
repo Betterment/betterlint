b994e43b38d177632628e3e4c2b3a47cb4e8a88b
Ray Zane <raymondzane@gmail.com>
2022-08-24 12:26:11 -0400

    Add a linter for hardcoded IDs in specs (#8)
    Hardcoded IDs lead to flaky specs. This PR adds a rule that will report the following offenses:
    
    ```ruby
    FactoryBot.create(:employee, id: 99)
    FactoryBot.create(:employee, employer_id: 99)
    ```
    
    It can also detect when an ID is hardcoded in a `let` and passed to a factory:
    
    ```ruby
    let(:employee_id) { 99 }
    let(:employee) { FactoryBot.create(:employee, id: employee_id) }
    ```

---
+++ lib/rubocop/cop/betterment/hardcoded_id.rb
@@ -0,0 +1,101 @@
+module RuboCop
+  module Cop
+    module Betterment
+      class HardcodedID < Base
+        include RangeHelp
+        extend AutoCorrector
+
+        MSG = 'Hardcoded IDs cause flaky tests. Use a sequence instead.'.freeze
+
+        # @!method key(node)
+        def_node_matcher :key, '/^id$|_id$/'
+
+        # @!method value(node)
+        def_node_matcher :value, '{int | (str /^\d+$/)}'
+
+        # @!method pair(node, value_pattern)
+        def_node_matcher :pair, '(pair (sym #key) %1)'
+
+        # @!method hardcoded_id?(node)
+        def_node_matcher :hardcoded_id?, '#pair(#value)'
+
+        # @!method references_hardcoded_id?(node, method_name)
+        def_node_matcher :references_hardcoded_id?, '#pair((send nil? %1))'
+
+        # @!method on_factory(node)
+        def_node_matcher :on_factory, <<~PATTERN
+          (send (const nil? :FactoryBot) {:create | :create_list} _factory ... (hash $...))
+        PATTERN
+
+        # @!method on_let_id(node)
+        def_node_matcher :on_let_id, <<~PATTERN
+          (block (send nil? {:let | :let!} (sym $#key)) _block_args $#value)
+        PATTERN
+
+        def on_send(node)
+          each_factory_attribute(node) do |attribute_node|
+            next unless hardcoded_id?(attribute_node)
+
+            add_offense(attribute_node) do |corrector|
+              attribute = Utils::HardcodedAttribute.new(attribute_node)
+              correct_factory_usage(corrector, attribute) if attribute.correctable?
+            end
+          end
+        end
+
+        def on_block(node)
+          on_let_id(node) do |name, value|
+            node.parent&.each_descendant do |child|
+              each_factory_attribute(child) do |attribute_node|
+                next unless references_hardcoded_id?(attribute_node, name)
+
+                add_offense(node) do |corrector|
+                  attribute = Utils::HardcodedAttribute.new(attribute_node)
+                  corrector.replace(attribute.node.value, value.value.to_s) if attribute.correctable?
+                end
+              end
+            end
+          end
+        end
+        alias on_numblock on_block
+
+        private
+
+        def each_factory_attribute(node, &block)
+          on_factory(node) do |attributes|
+            attributes.each(&block)
+          end
+        end
+
+        def correct_factory_usage(corrector, attribute)
+          corrector.remove(with_comma(attribute.node))
+
+          attribute.each_integer_reference do |reference|
+            corrector.replace(reference, attribute.replacement)
+          end
+
+          attribute.each_string_reference do |reference|
+            attribute.each_range_within_string(reference) do |range|
+              corrector.replace(range, "\#{#{attribute.replacement}}")
+            end
+
+            ensure_double_quoted(corrector, reference)
+          end
+        end
+
+        def ensure_double_quoted(corrector, node)
+          if node.source.start_with?("'")
+            corrector.replace(node.loc.begin, '"')
+            corrector.replace(node.loc.end, '"')
+          end
+        end
+
+        def with_comma(attribute)
+          range = attribute.location.expression
+          range = range_with_surrounding_space(range)
+          range_with_surrounding_comma(range, :left)
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/betterment/utils/hardcoded_attribute_spec.rb
@@ -0,0 +1,80 @@
+require 'spec_helper'
+
+describe RuboCop::Cop::Utils::HardcodedAttribute do
+  include RuboCop::AST::Sexp
+
+  subject { described_class.new(find_attribute) }
+
+  context "when the attribute is not within a let" do
+    let(:source) { 'FactoryBot.create(:user, id: 42)' }
+
+    it "is not correctable" do
+      expect(subject).not_to be_correctable
+    end
+  end
+
+  context "when the attribute is passed to create_list" do
+    let(:source) do
+      <<~RUBY
+        RSpec.describe User do
+          let(:user) { FactoryBot.create_list(:user, foo_id: 42) }
+        end
+      RUBY
+    end
+
+    it "is not correctable" do
+      expect(subject).not_to be_correctable
+    end
+  end
+
+  context "when the attribute is a foreign key within a let block" do
+    let(:source) do
+      <<~RUBY
+        RSpec.describe User do
+          let(:user) { FactoryBot.create(:user, foo_id: 42) }
+          specify { 42 }
+          specify { "Foo 42" }
+        end
+      RUBY
+    end
+
+    it "is not correctable" do
+      expect(subject).not_to be_correctable
+    end
+  end
+
+  context "when the attribute is an ID within a let block" do
+    let(:source) do
+      <<~RUBY
+        RSpec.describe User do
+          let(:user) { FactoryBot.create(:user, id: 42) }
+          specify { 42 }
+          specify { 'User 42' }
+        end
+      RUBY
+    end
+
+    it "is correctable" do
+      expect(subject).to be_correctable
+    end
+
+    it "finds integer references" do
+      expect(subject.enum_for(:each_integer_reference)).to match_array([s(:int, 42)])
+    end
+
+    it "finds string references" do
+      expect(subject.enum_for(:each_string_reference)).to match_array([s(:str, "User 42")])
+    end
+
+    it "finds ranges of string references" do
+      reference = subject.enum_for(:each_string_reference).first
+      range = subject.enum_for(:each_range_within_string, reference).first
+      expect(range).to have_attributes(begin_pos: 108, end_pos: 110)
+    end
+  end
+
+  def find_attribute
+    ast = RuboCop::AST::ProcessedSource.new(source, RUBY_VERSION.to_f).ast
+    ast.each_descendant(:pair).first
+  end
+end
