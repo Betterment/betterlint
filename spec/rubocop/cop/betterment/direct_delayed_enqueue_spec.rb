4a773b09c36904ec947bc4bb822e035cebd6a59b
Ray Zane <raymondzane@gmail.com>
2024-09-26 14:24:04 -0400

    Discourage the use of `Object#delay` and `Delayed::Job.enqueue` (#54)
    We want all of our jobs to use ActiveJob.

---
+++ spec/rubocop/cop/betterment/direct_delayed_enqueue_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe RuboCop::Cop::Betterment::DirectDelayedEnqueue, :config do
+  it "adds an offense when using `.delay` without arguments" do
+    expect_offense(<<~RUBY)
+      user.delay.save!
+      ^^^^^^^^^^ Please use Active Job instead of using `Object#delay`
+    RUBY
+  end
+
+  it "adds an offense when using `.delay` with arguments" do
+    expect_offense(<<~RUBY)
+      user.delay(foo: 'bar').save!
+      ^^^^^^^^^^^^^^^^^^^^^^ Please use Active Job instead of using `Object#delay`
+    RUBY
+  end
+
+  it "adds an offense when using `Delayed::Job.enqueue`" do
+    expect_offense(<<~RUBY)
+      Delayed::Job.enqueue(Job.new)
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Please use Active Job instead of using `Delayed::Job.enqueue`
+    RUBY
+  end
+end
