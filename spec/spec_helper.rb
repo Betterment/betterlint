5ed5a5e7d4597b60f84bdbcfde32acc10b536167
Peter Boling <peter.boling@gmail.com>
2024-12-20 15:47:21 -0700

    👷 Never load pry on CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'rubocop'
 require 'rubocop/cop/betterment'
 require 'rubocop/rspec/support'
 require 'support/betterlint_config'
-require 'pry'
+require 'pry' unless ENV.fetch('CI', 'false').casecmp?('true')
 
 RSpec.configure do |config|
   config.include RuboCop::RSpec::ExpectOffense
