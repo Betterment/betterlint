a01e5b41f18bb9444444c45506411a47d12a11a4
Ray Zane <raymondzane@gmail.com>
2025-01-27 09:25:53 -0500

    Fix NonStandardActions

--- lib/rubocop/cop/betterment/non_standard_actions.rb
+++ lib/rubocop/cop/betterment/non_standard_actions.rb
@@ -3,7 +3,7 @@
 module RuboCop
   module Cop
     module Betterment
-      class NonStandardActions < Cop
+      class NonStandardActions < Base
         MSG_GENERAL = 'Use a new controller instead of custom actions.'
         MSG_RESOURCE_ONLY = "Resource route refers to a non-standard action in it's 'only:' param. #{MSG_GENERAL}".freeze
         MSG_ROUTE_TO = "Route goes to a non-standard controller action. #{MSG_GENERAL}".freeze
--- spec/rubocop/cop/betterment/non_standard_actions_spec.rb
+++ spec/rubocop/cop/betterment/non_standard_actions_spec.rb
@@ -4,54 +4,43 @@ require 'spec_helper'
 
 describe RuboCop::Cop::Betterment::NonStandardActions, :betterlint_config do
   context 'when defining routes containing non standard actions' do
-    let(:source) do
-      <<-SRC
-      Rails.application.routes.draw do
-        # OK
-        resource :status, only: :show
-        resources :messages, only: [:index,:show,:create]
-        get :signup, to: 'signups#show'
-        get 'old_summary', to: redirect('/summary', status: 302)
-        resources :users do
-          get 'messages', to: 'user_messages#show', as: :user_messages
-        end
-        resource :react_app, controller: 'react_app', only: [] do
-          get '', action: :show
-          get '/*path', action: :show
-        end
-
-        # NOT OK
-        resources :items, only: :index do
-          get 'total', on: :member
-        end
-        resource :splash, only: [:show, :dismiss]
-        get 'summary', to: 'pages#summary'
-      end
-      SRC
-    end
-
     it 'registers offences' do
-      inspect_source(source)
-      expect(cop.highlights).to contain_exactly(
-        include("get 'total'"),
-        include('only: [:show, :dismiss]'),
-        include("to: 'pages#summary'"),
-      )
+      expect_offense(<<~RUBY)
+        Rails.application.routes.draw do
+          # OK
+          resource :status, only: :show
+          resources :messages, only: [:index,:show,:create]
+          get :signup, to: 'signups#show'
+          get 'old_summary', to: redirect('/summary', status: 302)
+          resources :users do
+            get 'messages', to: 'user_messages#show', as: :user_messages
+          end
+          resource :react_app, controller: 'react_app', only: [] do
+            get '', action: :show
+            get '/*path', action: :show
+          end
+
+          # NOT OK
+          resources :items, only: :index do
+            get 'total', on: :member
+            ^^^^^^^^^^^^^^^^^^^^^^^^ Route goes to a non-standard controller action[...]
+          end
+          resource :splash, only: [:show, :dismiss]
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Resource route refers to a non-standard action[...]
+          get 'summary', to: 'pages#summary'
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Route goes to a non-standard controller action[...]
+        end
+      RUBY
     end
   end
 
   context 'when not defining routes' do
-    let(:source) do
-      <<-SRC
-      def my_method
-        get :value
-      end
-      SRC
-    end
-
     it 'does not register offences' do
-      inspect_source(source)
-      expect(cop.offenses).to be_empty
+      expect_no_offenses(<<~RUBY)
+        def my_method
+          get :value
+        end
+      RUBY
     end
   end
 
@@ -61,17 +50,13 @@ describe RuboCop::Cop::Betterment::NonStandardActions, :betterlint_config do
         'AdditionalAllowedActions' => %w(update_all destroy_all),
       }
     end
-    let(:source) do
-      <<-SRC
-      Rails.application.routes.draw do
-        resources :messages, only: [:index, :create, :destroy, :destroy_all]
-      end
-      SRC
-    end
 
     it 'will allow those actions instead of the default list' do
-      inspect_source(source)
-      expect(cop.offenses).to be_empty
+      expect_no_offenses(<<~RUBY)
+        Rails.application.routes.draw do
+          resources :messages, only: [:index, :create, :destroy, :destroy_all]
+        end
+      RUBY
     end
   end
 end
