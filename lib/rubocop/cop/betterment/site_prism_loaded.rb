2043b36bd922e52d08afd16f526287104d2bd322
Ray Zane <raymondzane@gmail.com>
2025-01-27 08:47:48 -0500

    Update SitePrismLoaded

--- lib/rubocop/cop/betterment/site_prism_loaded.rb
+++ lib/rubocop/cop/betterment/site_prism_loaded.rb
@@ -3,22 +3,20 @@
 module RuboCop
   module Cop
     module Betterment
-      class SitePrismLoaded < Cop
+      class SitePrismLoaded < Base
+        extend AutoCorrector
+
         MSG = 'Use `be_loaded` instead of `be_displayed`'
 
-        def_node_matcher :be_displayed_call?, <<-PATTERN
-          (send (send nil? :expect _) _ (send nil? :be_displayed))
+        def_node_matcher :on_be_displayed, <<-PATTERN
+          (send (send nil? :expect _) _ $(send nil? :be_displayed))
         PATTERN
 
         def on_send(node)
-          return unless be_displayed_call?(node)
-
-          add_offense(node, location: node.children[2].source_range)
-        end
-
-        def autocorrect(node)
-          lambda do |corrector|
-            corrector.replace(node.children[2], 'be_loaded')
+          on_be_displayed(node) do |be_displayed|
+            add_offense(be_displayed) do |corrector|
+              corrector.replace(be_displayed, 'be_loaded')
+            end
           end
         end
       end
