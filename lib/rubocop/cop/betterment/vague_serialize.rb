e83b2322104b41fa9290100a854ce21f43468b9c
Ricardo Vela <ricardo.garza@betterment.com>
2025-06-12 13:25:03 -0600

    update vague_serialize and its specs

--- lib/rubocop/cop/betterment/vague_serialize.rb
+++ lib/rubocop/cop/betterment/vague_serialize.rb
@@ -4,18 +4,30 @@ module RuboCop
   module Cop
     module Betterment
       class VagueSerialize < Base
-        MSG = 'Active Record models with serialized columns should specify which ' \
-              'deserializer to use instead of falling back to the default.'
+        MSG = <<~MESSAGE
+          Active Record models with serialized columns should specify which deserializer to use instead of falling back to the default.
+
+          If you are using Rails <= 7.0, stick with a positional argument. E.g.
+            `serialize :foo, MyCoderClass`
+
+          If you are using Rails >= 7.1, use the `coder` kwarg, since the positional arg was deprecated
+            e.g. `serialize :foo, coder: MyCoderClass`
+        MESSAGE
 
         # @!method serialize?(node)
         def_node_matcher :serialize?, <<-PATTERN
           (send nil? :serialize ...)
         PATTERN
 
+        # @!method kwargs_with_coder?(node)
+        def_node_matcher :kwargs_with_coder?, '(hash <(pair (sym :coder) (const ...)) ...>)'
+
         def on_send(node)
           return unless serialize? node
 
-          add_offense(node) if node.arguments.length < 2 || !node.arguments[1].const_type?
+          coder_args = node.arguments[1..].select { |arg| arg.const_type? || kwargs_with_coder?(arg) }
+
+          add_offense(node) if node.arguments.length < 2 || coder_args.length != 1
         end
         alias on_csend on_send
       end
--- spec/rubocop/cop/betterment/vague_serialize_spec.rb
+++ spec/rubocop/cop/betterment/vague_serialize_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 describe RuboCop::Cop::Betterment::VagueSerialize, :config do
   let(:offense) do
-    "Active Record models with serialized columns should specify which deserializer to use instead of falling back to the default."
+    "Active Record models with serialized columns should specify which deserializer to use instead of falling back to the default.[...]"
   end
 
   context 'defining a class with a serialized column' do
@@ -17,10 +17,40 @@ describe RuboCop::Cop::Betterment::VagueSerialize, :config do
       RUBY
     end
 
+    it 'registers an offense if there is no deserializer specified with the coder arg' do
+      expect_offense(<<-RUBY)
+        class Cat < ActiveRecord::Base
+          serialize :attributes, type: MyType
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{offense}
+        end
+      RUBY
+    end
+
+    it 'registers an offense if a deserializer is specified with both positional args and kwargs' do
+      expect_offense(<<-RUBY)
+        class Cat < ActiveRecord::Base
+          serialize :attributes, Array, coder: Array
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{offense}
+        end
+      RUBY
+    end
+
     it 'does not register an offense if there is a deserializer' do
       expect_no_offenses(<<-RUBY)
         class Cat < ActiveRecord::Base
           serialize :attributes, Array
+        end
+      RUBY
+
+      expect_no_offenses(<<-RUBY)
+        class Cat < ActiveRecord::Base
+          serialize :attributes, coder: Array
+        end
+      RUBY
+
+      expect_no_offenses(<<-RUBY)
+        class Cat < ActiveRecord::Base
+          serialize :attributes, type: Array, coder: Array
         end
       RUBY
     end
